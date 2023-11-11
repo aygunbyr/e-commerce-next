48d76adb394d6c7e7c3446645c5c925941bed7dc
Aygün Bayır <turkoaygun@gmail.com>
2023-11-11 21:24:04 +0300

    fix: footer height

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="h-36 bg-zinc-900 p-4 text-zinc-100">
+    <footer className="min-h-fit bg-zinc-900 p-4 text-zinc-100">
       <div className="container mx-auto flex flex-col gap-5 max-xl:w-96">
         <span className="text-3xl">E-Commerce</span>
         <span className="text-lg">
