6e774f61d23cf55b2ea96d6ed5914da1542e3d97
Aygün Bayır <turkoaygun@gmail.com>
2024-07-20 15:46:52 +0300

    feat: footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,53 @@
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs';
+
 const Footer = () => {
   return (
-    <footer className="p-4 text-primary">
-      <div className="container flex flex-col gap-5 max-xl:w-96">
-        <span className="text-3xl">E-Commerce</span>
-        <span className="text-lg">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua.
-        </span>
-      </div>
+    <footer className="container my-10 p-5 text-lg text-primary-dark max-xl:w-96">
+      &copy; 2024 All rights reserved.
+      <nav className="flex flex-col gap-5 py-5 sm:flex-row sm:gap-10">
+        <ul className="mr-5 flex flex-col">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <ul className="mr-5 flex flex-col">
+          <li>
+            <a href="#">Electronics</a>
+          </li>
+          <li>
+            <a href="#">Jewelery</a>
+          </li>
+          <li>
+            <a href="#">Men&apos;s Clothing</a>
+          </li>
+          <li>
+            <a href="#">Women&apos;s Clothing</a>
+          </li>
+        </ul>
+        <ul className="flex gap-2">
+          <li>
+            <BsFacebook size={24} />
+          </li>
+          <li>
+            <BsInstagram size={24} />
+          </li>
+          <li>
+            <BsTwitter size={24} />
+          </li>
+          <li>
+            <BsLinkedin size={24} />
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 };
